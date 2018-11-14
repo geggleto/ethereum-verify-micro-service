86d0340f6b5c3185f0bc51acf3255a39637a12cc
Glenn Eggeton <geggleton@mobials.com>
2018-11-13 20:42:59 -0500

    Sigh

--- cron/app.js
+++ cron/app.js
@@ -7,7 +7,7 @@ const Web3 = require('web3');
 
 let web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/metamask'));
 
-let blockNumber = 6699844;
+let blockNumber = 6699843;
 
 //Process Transaction Hashes
 
